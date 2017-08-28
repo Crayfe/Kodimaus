7642f3ebb2a76719ecb8f1f5a73a595d4ef96ad8
Cristian Ayuso Ferrón <crayfe@gmail.com>
2017-08-28 13:56:18 +0200

    Initial commit

---
+++ app/src/main/java/crayfe/kodimaus/MainItemStoreActivity.java
@@ -0,0 +1,13 @@
+package crayfe.kodimaus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainItemStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_item_store);
+    }
+}
